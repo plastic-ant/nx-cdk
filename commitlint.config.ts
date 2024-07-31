bfc3f05985eee4dec5e491e39ac92c49a2e9b8a0
pas-mike <michael.devereux@plasticant.co.uk>
2024-07-31 16:01:16 +0000

    chore(docs): added example

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -5,7 +5,11 @@ const config = {
   rules: {
     "scope-empty": [2, "never"],
     "scope-enum": async (ctx) => {
-      return [2, "always", ["repo", "ci", ...utils.getProjects(ctx, ({ name, projectType }) => !name.includes("e2e"))]];
+      return [
+        2,
+        "always",
+        ["docs", "repo", "ci", ...utils.getProjects(ctx, ({ name, projectType }) => !name.includes("e2e"))],
+      ];
     },
   },
   // . . .
